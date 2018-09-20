b5dbbf7fbbc6ea1e10763a5d12040590d82ae469
Wycliff-Kenyoru <wycliffkerongo@gmail.com>
2018-09-20 10:52:16 +0300

    first commit

---
+++ contact.py
@@ -0,0 +1,37 @@
+class Contact:
+    def_init_(self, first_name,last_name,phone_number,email):
+
+    first_name: New contact first name.
+    last_name: New contact last name.
+    number:New contact phone number.
+    email : New contact email addresss.
+
+    class Contact:
+    """
+    Class that generates new instances of contacts.
+    """
+
+    def __init__(self,first_name,last_name,phone_number,email):
+
+      # docstring removed for simplicity
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = number
+        self.email = email
+
+        class Contact:
+    """
+    Class that generates new instances of contacts.
+    """
+
+    contact_list = [] # Empty contact list
+
+    def __init__(self,first_name,last_name,number,email):
+
+      # docstring removed for simplicity
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = number
+        self.email = email
